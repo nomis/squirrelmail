ffb41b671a58f8dfd9b6310297144d3409587485
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2020-01-07 07:42:38 +0000

    Happy New Year
    git-svn-id: https://svn.code.sf.net/p/squirrelmail/code/branches/SM-1_4-STABLE@14840 7612ce4b-ef26-0410-bec9-ea0150e637f0

--- squirrelmail/plugins/mail_fetch/class.POP3.php
+++ squirrelmail/plugins/mail_fetch/class.POP3.php
@@ -11,7 +11,7 @@
  *
  * POP3 class
  *
- * @copyright 1999-2019 The SquirrelMail Project Team
+ * @copyright 1999-2020 The SquirrelMail Project Team
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version $Id$
  * @package plugins
