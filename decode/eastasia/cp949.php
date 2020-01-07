b2d24dce0b036dac52ae5319f5641cb93c4a772f
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2020-01-07 07:45:48 +0000

    Happy New Year
    git-svn-id: https://svn.code.sf.net/p/squirrelmail/code/trunk@14841 7612ce4b-ef26-0410-bec9-ea0150e637f0

--- decode/eastasia/cp949.php
+++ decode/eastasia/cp949.php
@@ -29,7 +29,7 @@
  *  <li>johab ?
  * </ul>
  * 
- * @copyright (c) 2004-2019 The SquirrelMail Project Team
+ * @copyright (c) 2004-2020 The SquirrelMail Project Team
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  * @version $Id$
  * @package decode
