9a1fa5cb4495493dbf1c171e3c4de7481c87a772
pdontthink <pdontthink@7612ce4b-ef26-0410-bec9-ea0150e637f0>
2025-01-02 02:06:04 +0000

    Happy New Year
    git-svn-id: https://svn.code.sf.net/p/squirrelmail/code/branches/SM-1_4-STABLE@15030 7612ce4b-ef26-0410-bec9-ea0150e637f0

--- squirrelmail/plugins/listcommands/mailout.php
+++ squirrelmail/plugins/listcommands/mailout.php
@@ -2,7 +2,7 @@
 /**
  * mailout.php
  *
- * Copyright (c) 1999-2024 The SquirrelMail Project Team
+ * Copyright (c) 1999-2025 The SquirrelMail Project Team
  * Licensed under the GNU GPL. For full terms see the file COPYING.
  *
  * $Id$
